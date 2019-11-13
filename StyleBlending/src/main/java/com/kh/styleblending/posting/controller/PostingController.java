81aa0f79ee3df176bd97fb9e32ee2ab2622f7590
jangy <jangy@DESKTOP-UIBTLAC>
2019-11-13 14:27:24 +0900

    insert p

--- StyleBlending/src/main/java/com/kh/styleblending/posting/controller/PostingController.java
+++ StyleBlending/src/main/java/com/kh/styleblending/posting/controller/PostingController.java
@@ -2,9 +2,13 @@ package com.kh.styleblending.posting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.styleblending.posting.model.service.PostingService;
+import com.kh.styleblending.posting.model.vo.Posting;
+import com.kh.styleblending.posting.model.vo.Style;
 
 @Controller
 public class PostingController {
@@ -18,10 +22,30 @@ public class PostingController {
 		return "posting/info";
 	}
 	
-	@RequestMapping("pInsert")
-	public String insert() {
+	// 포스팅등록
+	@RequestMapping("pInsertForm.do")
+	public String insertForm() {
 		return "posting/insert";
 	}
+	
+	// 포스팅등록 처리
+	@RequestMapping("pInsert.do")
+	public ModelAndView insert(Posting po, Style st, ModelAndView mv) {
+		
+		int result = pService.insertPosting(po, st);
+		
+		if(result > 0) {
+			mv.setViewName("redirect:home.do");
+		}else {
+			mv.addObject("msg", "포스팅 등록 실패").setViewName("common/errorPage");
+		}
+		
+		return mv;
+	}
+	
+	
+	
+	
 	
 	
 	
