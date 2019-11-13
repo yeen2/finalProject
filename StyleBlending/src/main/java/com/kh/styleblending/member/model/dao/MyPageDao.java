bb97cc45a1dbd7678946ed7e705b8911e77cba30
hansadong <hansadong@DESKTOP-KAV9VTP>
2019-11-13 20:42:22 +0900

    마이페이지 수정12

--- StyleBlending/src/main/java/com/kh/styleblending/member/model/dao/MyPageDao.java
+++ StyleBlending/src/main/java/com/kh/styleblending/member/model/dao/MyPageDao.java
@@ -1,5 +1,57 @@
 package com.kh.styleblending.member.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.styleblending.admin.model.vo.Ad;
+import com.kh.styleblending.member.model.vo.Member;
+import com.kh.styleblending.posting.model.vo.Posting;
+
+@Repository("mpDao")
 public class MyPageDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public Member selectProfile(Member m) {
+		return sqlSession.selectOne("mypageMapper.selectProfile", m);
+	}
+
+	public ArrayList<Posting> selectPostingList(Member m) {
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectPostingList", m);
+	}
+
+	public ArrayList<Posting> selectLikeList(Member m) {
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectLikeList", m);
+	}
+
+	public ArrayList<Member> selectFanList(Member m) {
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectFanList", m);
+	}
+
+	public ArrayList<Member> selectFollowingList(Member m) {
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectFollowingList", m);
+	}
+
+	public int updateProfile(Member m) {
+		return sqlSession.update("mypageMapper.updateProfile", m);
+	}
+
+	public ArrayList<Ad> selectAdList(Member m) {
+		return (ArrayList)sqlSession.selectList("mypageMapper.selectAdList", m);
+	}
+
+	public int updatePass(Member m) {
+		return sqlSession.update("mypageMapper.updatePass", m);
+	}
+
+	public int deleteMember(String email) {
+		return sqlSession.update("mypageMapper.deleteMember", email);
+	}
+	
+	
 
 }
