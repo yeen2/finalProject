227c1c737b30939fa9bc9c68756170d35e539158
hansadong <hansadong@DESKTOP-KAV9VTP>
2019-11-29 17:06:48 +0900

    해시검색 수정, hash vo, mybatis-config 수정

---
+++ StyleBlending/src/main/java/com/kh/styleblending/posting/model/vo/Hash.java
@@ -0,0 +1,20 @@
+package com.kh.styleblending.posting.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Hash {
+	
+	private int hano;
+	private int pno;
+	private String hashtag;
+
+}
